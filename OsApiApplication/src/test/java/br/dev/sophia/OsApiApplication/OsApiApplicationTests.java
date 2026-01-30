d9ff73f4d8ab07fd696f0ee44a86a9f90b7c972c
sophiasoquecomph <sophia.med2008@gmail.com>
2026-01-30 08:36:39 -0300

    PDF 1 - Fase 1 e 2

---
+++ OsApiApplication/src/test/java/br/dev/sophia/OsApiApplication/OsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.dev.sophia.OsApiApplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
